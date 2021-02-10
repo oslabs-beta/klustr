b758ecdcc7c3c531df98d369fcd915c069331d6c
Eric <erictacher@gmail.com>
2021-02-10 16:25:05 -0500

    topics and partition components coded

--- client/components/TopicBox.jsx
+++ client/components/TopicBox.jsx
@@ -5,11 +5,17 @@ function TopicBox({ topics }) {
   // prop drill the Brokers Array from Metrics Container?
   // or hook in directly?
 
+  const topicsList = []
+  
+  topics.map((topic) => {
+    topicsList.push(<Topics key={topic} topicname={topic} /> );
+  })
+
   return (
     <div>
       <h4>TOPICS</h4>
-      <span>Number of Topics</span>
-      <h1>{topics}</h1>
+      <span>Number of Topics {topics.length}</span>
+      <h1>{topicsList}</h1>
     </div>
   );
 }
--- client/components/Topics.jsx
+++ client/components/Topics.jsx
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import Partition from './Partition.jsx'
 
-function Topic() {
+function Topic({ topicname }) {
   
+  const [offsets, setOffsets] = useState([])
+
+  const fetchOffsets = () => {
+    fetch(`/admin/partitionInfo/${topicname}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setOffsets(data.offsets);
+      }) // returns an array of partition objects ([{"partition": 0,"offset": "21"}])
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    fetchOffsets();
+  }, []);
+
+  const handleClick = () => {
+    // trigger Partition popup
+  }
+
+
   return (
     <div>
-      <span>BrokerID: {brokerID}</span>
-      <span>Host: {host}</span>
-      <span>Port: {port}</span>
+      <h1>Topic: {topicname}</h1>
+      <Partition offsets={offsets} />
     </div>
   );
 }
