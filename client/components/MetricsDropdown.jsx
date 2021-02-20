7e7f6e85e162a750d87404cfe4dca071cde964c2
shahprose <72280598+shahprose@users.noreply.github.com>
2021-02-20 13:18:16 -0500

    metrics dropdown

--- client/components/MetricsDropdown.jsx
+++ client/components/MetricsDropdown.jsx
@@ -1,7 +1,55 @@
 import React from 'react';
+import Checkbox from '@material-ui/core/Checkbox';
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import CheckBoxOutlineBlankIcon from '@material-ui/icons/CheckBoxOutlineBlank';
+import CheckBoxIcon from '@material-ui/icons/CheckBox';
 
-const MetricsDropdown = () => {
-  return <div>Dropdown here</div>;
-};
+const icon = <CheckBoxOutlineBlankIcon fontSize='small' />;
+const checkedIcon = <CheckBoxIcon fontSize='small' />;
+
+function MetricsDropdown() {
+  return (
+    <Autocomplete
+      multiple
+      id='checkboxes-tags-demo'
+      options={selectedMetrics}
+      disableCloseOnSelect
+      getOptionLabel={(option) => option.title}
+      renderOption={(option, { selected }) => (
+        <React.Fragment>
+          <Checkbox
+            icon={icon}
+            checkedIcon={checkedIcon}
+            style={{ marginRight: 8 }}
+            checked={selected}
+          />
+          {option.title}
+        </React.Fragment>
+      )}
+      style={{ width: 500 }}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          variant='outlined'
+          label='Checkboxes'
+          placeholder='Favorites'
+        />
+      )}
+    />
+  );
+}
+
+const selectedMetrics = [
+  { title: 'brokertopicmetrics_bytesin_total' },
+  { title: 'brokertopicmetrics_bytesout_total' },
+  { title: 'cpu_seconds_total' },
+  { title: 'disk_write_bytes' },
+  { title: 'disk_read_bytes' },
+];
+
+// const MetricsDropdown2 = () => {
+//   return <div>Dropdown here</div>;
+// };
 
 export default MetricsDropdown;
