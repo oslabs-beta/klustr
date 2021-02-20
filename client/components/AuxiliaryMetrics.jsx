24da3be928bd809b9647d709a4079c0193d90b96
Eric <erictacher@gmail.com>
2021-02-19 19:42:20 -0500

    fetching aux metrics on front end

--- client/components/AuxiliaryMetrics.jsx
+++ client/components/AuxiliaryMetrics.jsx
@@ -2,8 +2,19 @@ import React from 'react';
 import DonutChartMetrics from './DonutChartMetrics.jsx';
 import LineGraphMetrics from './LineGraphMetrics.jsx';
 
-const AuxiliaryMetrics = () => {
-  return <div> Auxiliary Metrics here </div>;
+const AuxiliaryMetrics = ({ metrics }) => {
+  // const metricNames = Object.keys(metrics)
+  // const graphs = metricNames.forEach( ele => {
+
+  // } )
+  console.log('new passed down state', metrics);
+  return (
+    <div>
+      <h4>disk write bytes</h4>
+      {/* {metrics} */}
+      {/* <LineGraphMetrics /> */}
+    </div>
+  );
 };
 
 export default AuxiliaryMetrics;
